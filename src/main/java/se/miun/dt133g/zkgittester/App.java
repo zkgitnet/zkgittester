c417d58c8f674bad8215c2d8cf7837b27e4fbec5
lero2301 <lero2301@student.miun.se>
2025-02-09 19:40:26 +0100

    initial commit

---
+++ src/main/java/se/miun/dt133g/zkgittester/App.java
@@ -0,0 +1,10 @@
+package se.miun.dt133g.zkgittester;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/se/miun/dt133g/zkgittester/AppTest.java
@@ -0,0 +1,19 @@
+package se.miun.dt133g.zkgittester;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
